b15bb613732c8b0c92999850af1a910894cb4f6f
luiz note <luizsilvacaetano192@gmail.com>
2025-04-06 13:50:11 -0300

    atualiza

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -15,19 +15,14 @@ class DriverController extends Controller
 
     public function getData()
     {
-        $drivers = Driver::select(['id', 'name', 'phone', 'license_number','address','identity_card']);
+        $query = Driver::query();
 
-        return DataTables::of($drivers)
-            ->addColumn('actions', function ($driver) {
-                return '
-                    <a href="'.route('shipments.index', ['driver_id' => $driver->id]).'" class="btn btn-primary btn-sm">🚚 Ver Fretes</a>
-                   
-                ';
-            })
-
-            
-            ->rawColumns(['actions'])
-            ->make(true);
+        return DataTables::of($query)
+            ->addColumn('driver_license_front', fn($driver) => $driver->driver_license_front_url)
+            ->addColumn('driver_license_back', fn($driver) => $driver->driver_license_back_url)
+            ->addColumn('face_photo', fn($driver) => $driver->face_photo_url)
+            ->addColumn('address_proof', fn($driver) => $driver->address_proof_url)
+            ->toJson();
     }
 
     public function data(Request $request)
