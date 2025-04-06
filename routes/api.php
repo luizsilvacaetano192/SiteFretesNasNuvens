06a3894b26038081676ddaa9a4d2e47f851e9162
luiz note <luizsilvacaetano192@gmail.com>
2025-04-06 16:24:16 -0300

    atualiza

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::post('/api/analyze-driver', [DriverAnalysisController::class, 'analyze']);
+Route::post('/analyze-driver', [DriverAnalysisController::class, 'analyze']);
 
 
 Route::get('/shipments', [ShipmentController::class, 'getAllShipments']);
