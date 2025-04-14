7743180af7ed2c43827c91f9884bdb75bfdd09b4
luiz note <luizsilvacaetano192@gmail.com>
2025-04-14 19:04:26 -0300

    atualiza

--- app/Http/Controllers/FreightController.php
+++ app/Http/Controllers/FreightController.php
@@ -308,10 +308,8 @@ class FreightController extends Controller
             ]);
 
             $data = $response->json();
-
-            dd( $data);
          
-            if ($data->success) {
+            if ($data['success']) {
                
                 $paymentData = [
                     'payment_link' => $data->asaasResponse['url'] ?? null,
