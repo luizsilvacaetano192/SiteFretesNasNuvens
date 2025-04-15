a5de123afe1841d9a2ea6dc0b8042627c4b37400
luiz note <luizsilvacaetano192@gmail.com>
2025-04-15 14:48:34 -0300

    atualiza

--- resources/views/settings/create.blade.php
+++ resources/views/settings/create.blade.php
@@ -282,7 +282,11 @@
 @endpush
 
 @push('scripts')
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>  
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 <script>
+ 
 // Versão robusta que verifica todas as dependências
 document.addEventListener('DOMContentLoaded', function() {
     // Verifica se jQuery está carregado
