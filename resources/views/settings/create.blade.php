dc2ed272b7d24755f075989b25e3adaef14ff0cd
luiz note <luizsilvacaetano192@gmail.com>
2025-04-15 14:34:37 -0300

    atualiza

--- resources/views/settings/create.blade.php
+++ resources/views/settings/create.blade.php
@@ -175,6 +175,7 @@
 @endsection
 
 @push('styles')
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" rel="stylesheet">
 <style>
 .divider {
