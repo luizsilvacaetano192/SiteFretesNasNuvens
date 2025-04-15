8d897baf96967de1f9d8ee4da851c69e75f0f087
luiz note <luizsilvacaetano192@gmail.com>
2025-04-15 14:31:30 -0300

    atualiza

--- resources/views/settings/create.blade.php
+++ resources/views/settings/create.blade.php
@@ -284,6 +284,7 @@
 @push('scripts')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script>
 $(document).ready(function() {
     // Configuração global do Toastr
