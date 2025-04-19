d4ad698aebbf57538be40e927353e4fc8d0ed850
luiz note <luizsilvacaetano192@gmail.com>
2025-04-19 16:05:27 -0300

    atualiza

--- resources/views/drivers/index.blade.php
+++ resources/views/drivers/index.blade.php
@@ -27,9 +27,13 @@
 </div>
 
 <!-- Modal de Imagem Ampliada -->
-<div class="modal fade" id="imageModal" tabindex="-1">
+<!-- Modal de Imagem Ampliada -->
+<div class="modal fade" id="imageModal" tabindex="-1" aria-hidden="true" style="z-index: 1080;">
   <div class="modal-dialog modal-dialog-centered modal-xl">
     <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
       <div class="modal-body text-center p-0">
         <img id="modalImage" src="" class="img-fluid w-100" style="max-height:90vh; object-fit:contain;">
       </div>
