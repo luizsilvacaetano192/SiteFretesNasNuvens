2c9fbdad3413b29c3a641e2cf578c7bdad2b909d
luiz note <luizsilvacaetano192@gmail.com>
2025-04-28 13:23:49 -0300

    atualiza

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -286,7 +286,7 @@ class DriverController extends Controller
 
     public function destroy($id)
     {
-        $driver = Driver::findOrfail($id);
+        $driver = Driver::findOrFail($id);
         $driver->delete();
         return redirect()->route('drivers.index')->with('success', 'Driver deleted successfully.');
     }
