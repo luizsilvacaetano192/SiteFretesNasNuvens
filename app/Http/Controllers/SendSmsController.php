4c889a8b084cafd949baef194e7eabe4b481f74a
luiz note <luizsilvacaetano192@gmail.com>
2025-04-28 16:36:34 -0300

    atualiza

--- app/Http/Controllers/SendSmsController.php
+++ app/Http/Controllers/SendSmsController.php
@@ -10,18 +10,31 @@ class SendSmsController extends Controller
     public function store(Request $request)
     {
         $body = json_encode([
-            'phone_number' => $request->phone,
+            'phone' => $request->phone,
             'message' => $request->message
         ]);
 
         $response = Http::withHeaders([
             'Content-Type' => 'application/json',
-            'Accept' => 'application/json'
+            'Accept' => 'application/json',
         ])->post('https://2saeuklnwb.execute-api.us-east-1.amazonaws.com/teste/', $body);
 
         if ($response->successful()) {
-            return response()->json($response->json());
+            // Aqui decodifica o "body" interno
+            $data = $response->json();
+
+            // Se o "body" vier como string JSON, decodifica ele também:
+            if (isset($data['body'])) {
+                $bodyDecoded = json_decode($data['body'], true);
+
+                // Retorna o conteúdo certo para o front
+                return response()->json($bodyDecoded);
+            }
+
+            // Se por algum motivo não existir body:
+            return response()->json($data);
         }
+
         return response()->json([
             'success' => false,
             'message' => $response->body() ?: 'Erro ao conectar com o serviço de sms'
