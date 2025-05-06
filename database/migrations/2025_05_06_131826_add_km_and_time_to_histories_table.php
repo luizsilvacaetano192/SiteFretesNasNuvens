9c053c5aba4a4f11be76476f5a3badf166a8a4e7
luiz note <luizsilvacaetano192@gmail.com>
2025-05-06 10:20:23 -0300

    campos de km e time historie

---
+++ database/migrations/2025_05_06_131826_add_km_and_time_to_histories_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('histories', function (Blueprint $table) {
+            $table->float('km')->nullable();
+            $table->time('time_end')->nullable();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('histories', function (Blueprint $table) {
+            $table->dropColumn(['km', 'time_end']);
+        });
+    }
+};
