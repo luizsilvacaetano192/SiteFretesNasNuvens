f8a64456694d89b2f227c408f0361d69e52d0231
luiz note <luizsilvacaetano192@gmail.com>
2025-05-07 20:47:55 -0300

    campos de km e time historie

--- resources/views/freights/map.blade.php
+++ resources/views/freights/map.blade.php
@@ -133,7 +133,7 @@
                                 </tr>
                             </thead>
                             <tbody id="activity-history">
-                                @forelse($freight->history()->orderBy('id', 'asc')->get() as $location)
+                                @forelse($freight->history() as $location)
                                 <tr>
                                     <td>{{ \Carbon\Carbon::parse($location->date)->format('d/m/Y') }}</td>
                                     <td>{{ \Carbon\Carbon::parse($location->date)->format('H:i') }}</td>
