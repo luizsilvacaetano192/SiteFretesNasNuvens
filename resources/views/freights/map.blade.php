13d06d80f62708c3ed8a8a1d0dfec6888e0319f5
luiz note <luizsilvacaetano192@gmail.com>
2025-05-07 19:17:37 -0300

    campos de km e time historie

--- resources/views/freights/map.blade.php
+++ resources/views/freights/map.blade.php
@@ -73,7 +73,7 @@
                     <div id="map-container" style="position: relative;">
                         <!-- Controles do Mapa -->
                         <div id="map-controls" class="position-absolute top-0 end-0 mt-2 me-2" 
-                        style="z-index: 1000; margin-top: 100px;">
+                        style="z-index: 1000; margin-top: 400px;">
                             <div class="btn-group-vertical shadow-sm">
                                 <button id="track-toggle" class="btn btn-sm btn-primary">
                                     <i class="fas fa-lock"></i> Travar Mapa
