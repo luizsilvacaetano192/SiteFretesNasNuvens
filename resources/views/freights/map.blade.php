ee91bf195e50b2be05ed45e25882f2ba215c775a
luiz note <luizsilvacaetano192@gmail.com>
2025-05-10 12:14:54 -0300

    campos de km e time historie

--- resources/views/freights/map.blade.php
+++ resources/views/freights/map.blade.php
@@ -257,6 +257,7 @@
 @endpush
 
 @push('scripts')
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
 <script src="https://unpkg.com/leaflet-routing-machine@3.2.12/dist/leaflet-routing-machine.js"></script>
 <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
