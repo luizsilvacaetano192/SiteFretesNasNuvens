ddd7e5b7956c94a4a3d7a3510f756300f3f43ae4
luiz note <luizsilvacaetano192@gmail.com>
2025-05-17 00:15:24 -0300

    campos de km e time historie

--- resources/views/freights/map.blade.php
+++ resources/views/freights/map.blade.php
@@ -12,7 +12,7 @@
             </h1>
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
+                    <li class="breadcrumb-item"><a href="{{ route('freights.dashboard') }}">Dashboard</a></li>
                     <li class="breadcrumb-item"><a href="{{ route('freights.index') }}">Fretes</a></li>
                     <li class="breadcrumb-item active" aria-current="page">Rota</li>
                 </ol>
