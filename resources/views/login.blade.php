58e966f004d992ada4e29464d945af3d289d0efb
luiz note <luizsilvacaetano192@gmail.com>
2025-05-18 16:17:18 -0300

    favicon

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+        <!-- Favicon -->
+    <link rel="icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
     <title>Login - Fretes em Nuvens</title>
     <style>
         * {
