c72611fed34c5d1308811d1a1a2a7c650aa01e17
Luiz caetano <luizsilvacaetano192@gmail.com>
2025-07-27 21:56:05 -0300

    atualiza

--- resources/views/drivers/index.blade.php
+++ resources/views/drivers/index.blade.php
@@ -1501,12 +1501,13 @@ function format(d) {
 // Map Functions
 function showDriversLocation() {
     console.log('show drivers chamados');
+    initializeMap();
     if (!driversLocationModal) {
         driversLocationModal = new bootstrap.Modal('#driversLocationModal');
         
         // Adiciona listener para quando o modal estiver totalmente mostrado
        // $('#driversLocationModal').on('shown.bs.modal', function() {
-            initializeMap();
+            
      //   });
     }
     
